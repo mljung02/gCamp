e454f2b4d66902c6991d8c04f15304061c4d1d6b
Sam Genduso <sam.genduso@gmail.com>
2015-09-22 15:50:14 -0600

    tasks fully crud-ed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,52 @@
 class TasksController < ApplicationController
-  
+
   def index
+    @tasks = Task.all
   end
-  
+
   def new
     @task = Task.new
   end
-  
+
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to task_path(@task)
+      80.times {p '*'}
+      p 'hello'
+      redirect_to task_path(@task), notice: 'Task successfully created!'
     else
-      render new
+      render :action => :new
     end
-    
+
   end
-  
+
   def show
     @task = Task.find(params[:id])
   end
-  
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Task successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
-  
+
   def task_params
     params.require(:task).permit(:description)
   end
-  
+
 end
